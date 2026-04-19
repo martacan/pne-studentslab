958bce1c7478039a85518176dd10498b35d8b92d
martacan <m.cantelar.2025@alumnos.urjc.es>
2026-04-19 20:46:35 +0200

    changes made

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -2,55 +2,68 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
-import jinja2 as j
+import jinja2 as template
+from Seq1 import Seq
+from urllib.parse import parse_qs, urlparse
 
 
 
-# Define the Server's port
+
 PORT = 8080
-
-# -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 
-
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
-
     def do_GET(self):
         """This method is called whenever the client invokes the GET method
         in the HTTP protocol request"""
 
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
-        from urllib.parse import parse_qs, urlparse
+
         url_path = urlparse(self.path)
-        path = url_path.path  # we get it from here
+        path = url_path.path
         arguments = parse_qs(url_path.query)
 
+        #PART 1- PING METHOD
         if path == "/":
             contents = Path('html/index.html').read_text()
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
 
-        elif path.startswith("/ping"):
+        elif path == "/ping":
             contents = Path('html/ping.html').read_text()
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
+        #PART 2- GET METHOD
+        elif path == "/get":
+            sequences = {
+                "0": Seq("AAGTCAACCUUCC"),
+                "1": Seq("CCGTAAAACGT"),
+                "2": Seq("TTUATCCGGGG"),
+                "3": Seq("AGCCTAAACGAT"),
+                "4": Seq("AGTTCCGATTACG")
+            }
+            seq_number = arguments.get["seq_number"]
+            contents = Path("html/get.html").read_text()
+
+
+
+
         else:
             contents = Path('html/error.html').read_text()
 
 
-        # Open the form1.html file
-        # Read the index from the file
 
-        # Generating the response message
         self.send_response(200)  # -- Status line: OK!
-
-        # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
-
-        # The header is finished
         self.end_headers()
-
-        # Send the response message
         self.wfile.write(str.encode(contents))
 
         return
