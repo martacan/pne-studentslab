da4340855e41816035f6828b542783ea1f9a018d
martacan <m.cantelar.2025@alumnos.urjc.es>
2026-04-20 12:58:28 +0200

    changes made

---
+++ P06/Seq1.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+class Seq:
+    def __init__(self, strbases=None):
+        if strbases is None :
+            self.strbases = "NULL"
+            print("NULL sequence was created")
+            return
+        if all(base in "ACGT" for base in strbases):
+            self.strbases = strbases
+            print("New sequence was created")
+        else:
+            self.strbases = "ERROR"
+            print("INVALID sequence!")
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return 0
+        return len(self.strbases)
+
+    def count_base(self, base):
+        if self.strbases in ["NULL", "ERROR"]:
+            return 0
+        return self.strbases.count(base)
+
+    def count(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return {"A": 0, "T": 0, "G": 0, "C": 0}
+        return {base: self.strbases.count(base) for base in "ATCG"}
+
+    def reverse(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return self.strbases
+        return self.strbases[::-1]
+
+    def complement(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return self.strbases
+        complement_bases = str.maketrans("ATGC", "TACG")
+        return self.strbases.translate(complement_bases)
+
+    def read_fasta(self, filename):
+        file_path = Path(filename)
+        if not file_path.exists():
+            print(f"File{filename} does not exist")
+            self.strbases = "ERROR"
+            return self.strbases
+        file_content = file_path.read_text()
+        lines = file_content.split("\n")
+        self.strbases = "".join(lines[1:]).replace("\n", "")
+        return self.strbases
+    def most_frequent_base(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return "ERROR"
+        base_counts = self.count()
+        return max(base_counts, key=base_counts.get)
+
+
--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -33,6 +33,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(str.encode(contents))
 
+
         elif path == "/ping":
             contents = Path('html/ping.html').read_text()
             self.send_response(200)  # -- Status line: OK!
@@ -43,30 +44,121 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         #PART 2- GET METHOD
         elif path == "/get":
             sequences = {
-                "0": Seq("AAGTCAACCUUCC"),
-                "1": Seq("CCGTAAAACGT"),
-                "2": Seq("TTUATCCGGGG"),
-                "3": Seq("AGCCTAAACGAT"),
-                "4": Seq("AGTTCCGATTACG")
+                "0": "AAGTCAACCUUCC",
+                "1": "CCGTAAAACGT",
+                "2": "TTUATCCGGGG",
+                "3": "AGCCTAAACGAT",
+                "4": "AGTTCCGATTACG"
             }
-            seq_number = arguments.get["seq_number"]
-            contents = Path("html/get.html").read_text()
+
+            #parse convierte la url en un diccionario [arguments] y esta [] porque podria haber multiples valores lista, el .get busca una clave y devuelve una lista por eso se usa [0[ acceder al primer elemtno lista
+            seq_number = arguments.get("seq_number", ["0"])[0]
 
 
+            #acceder al valor en el diccionario de sequences
+            seq_value = sequences[seq_number]
 
+            #render
+            template_loader = template.FileSystemLoader("html")
+            template_env = template.Environment(loader=template_loader)
+            template_obj = template_env.get_template("get.html")
+            contents = template_obj.render(seq_number=seq_number, seq_value=seq_value)
+
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
+
+
+        elif path == "/gene":
+
+            selected_gene = arguments.get("gene", [""])[0]
+
+            filepath = Path(f"genes/{selected_gene}.txt")
+            try:
+                gene_sequence = filepath.read_text().strip()
+
+                if "\n" in gene_sequence:
+                    lines = gene_sequence.split("\n")
+                    if lines[0].startswith(">"):
+                        gene_sequence = "\n".join(lines[1:])
+
+
+                template_loader = template.FileSystemLoader("html")
+                template_env = template.Environment(loader=template_loader)
+                template_obj = template_env.get_template("gene.html")
+                contents = template_obj.render(gene_name=selected_gene, gene_sequence=gene_sequence)
+
+                self.send_response(200)  # -- Status line: OK!
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', len(str.encode(contents)))
+                self.end_headers()
+                self.wfile.write(str.encode(contents))
+
+            except FileNotFoundError:
+                contents = Path('html/error.html').read_text()
+                self.send_response(200)  # -- Status line: OK!
+                self.send_header('Content-Type', 'text/html')
+                self.send_header('Content-Length', len(str.encode(contents)))
+                self.end_headers()
+                self.wfile.write(str.encode(contents))
+
+        elif path == "/operation":
+            sequence = arguments.get("sequence", [""])[0]
+            operation = arguments.get("operation", [""])[0]
+
+            seq_obj = Seq(sequence)
+
+            if operation == "info":
+                lenght = seq_obj.len()
+
+                count_a = seq_obj.count_base("A")
+                count_c = seq_obj.count_base("C")
+                count_g = seq_obj.count_base("G")
+                count_t = seq_obj.count_base("T")
+
+                if lenght > 0:
+                    perc_a = round((count_a / lenght) * 100, 2)
+                    perc_c = round((count_c / lenght) * 100, 2)
+                    perc_g = round((count_g / lenght) * 100, 2)
+                    perc_t = round((count_t / lenght) * 100, 2)
+                else:
+                    perc_a = perc_c = perc_g = perc_t = 0
+
+                result = f"""
+-Total lenght: {lenght}<br>
+-A: {count_a} ({perc_a}%)<br>
+-C: {count_c} ({perc_c}%)<br>
+-G: {count_g} ({perc_g}%)<br>
+-T: {count_t} ({perc_t}%)"""
+
+            elif operation == "comp":
+                result = seq_obj.complement()
+            elif operation == "rev":
+                result = seq_obj.reverse()
+            else:
+                result = "Invalid Operation"
+
+            template_loader = template.FileSystemLoader("html")
+            template_env = template.Environment(loader=template_loader)
+            template_obj = template_env.get_template("operation.html")
+            contents = template_obj.render(sequence=sequence, operation=operation, result=result)
+
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
 
         else:
             contents = Path('html/error.html').read_text()
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
 
-
-
-        self.send_response(200)  # -- Status line: OK!
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(str.encode(contents)))
-        self.end_headers()
-        self.wfile.write(str.encode(contents))
-
-        return
 
 
 # ------------------------
